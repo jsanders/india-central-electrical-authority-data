920d8bb3266500a6a4b8678afe40921c106db493
James Sanders <sanderjd@gmail.com>
2013-09-20 10:42:49 -0600

    Script to download all coal, gas, and hydro data

---
+++ get_data.sh
@@ -0,0 +1,17 @@
+# All of the years for which any data exists for any type of data
+for year in 08 09 10 11 12 13
+do
+  # All possible month names - for some reason gas uses "april" instead of "apr"
+  for month in jan feb mar apr april may jun jul aug sep oct nov dec
+  do
+    wget --no-clobber http://cea.nic.in/archives/coal_monthly/${month}${year}.pdf -O coal/${month}${year}.pdf >> out.log 2>&1
+    wget --no-clobber http://www.cea.nic.in/reports/monthly/fuel_sup_consm_rep/gas%20based/${month}${year}.pdf -O gas/${month}${year}.pdf >> out.log 2>&1
+    wget --no-clobber http://cea.nic.in/archives/hydro_reserve/${month}${year}.pdf -O hydro/${month}${year}.pdf >> out.log 2>&1
+  done
+done
+
+# Delete any empty files which we get if we don't have a particular type of
+# data for a given month name of a given year
+find coal -empty -exec rm {} \;
+find gas -empty -exec rm {} \;
+find hydro -empty -exec rm {} \;
